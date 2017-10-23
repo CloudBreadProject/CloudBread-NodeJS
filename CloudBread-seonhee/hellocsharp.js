4ce7a7fc3d400030d314e973c8b14f91ed25d676
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-10-23 17:13:34 +0900

    remove CloudBread-seonhee

--- CloudBread-seonhee/hellocsharp.js
+++
@@ -1,8 +0,0 @@
-var edge = require('edge');
-
-var helloWorld = edge.func('async (input) => { return ".NET Welcomes " + input.ToString(); }');
-
-helloWorld('JavaScript', function (error, result) {
-  if (error) throw error;
-  console.log(result);
-})
